385b3bc9af9033ae976cf4157e1f8a83bc765a26
Kazheng <KahKitZheng@gmail.com>
2021-03-24 15:42:01 +0100

    Add a hardcoded version of the home screen

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import colors from "./utils/colors";
+import fakeData from "./fakeData";
+import TodoList from "./components/TodoList";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ flexDirection: "row" }}>
+        <View style={styles.divider} />
+        <Text style={styles.title}>
+          Todo{" "}
+          <Text style={{ fontWeight: "300", color: colors.blue }}>Lists</Text>
+        </Text>
+        <View style={styles.divider} />
+      </View>
+
+      <View style={{ marginVertical: 48 }}>
+        <TouchableOpacity style={styles.addList}>
+          <AntDesign name="plus" size={16} color={colors.blue} />
+        </TouchableOpacity>
+        <Text style={styles.add}>Add List</Text>
+      </View>
+
+      <View style={{ height: 275, paddingLeft: 32 }}>
+        <FlatList
+          data={fakeData}
+          keyExtractor={(item) => item.name}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item }) => <TodoList list={item} />}
+        />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +49,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  divider: {
+    backgroundColor: colors.lightBlue,
+    height: 1,
+    flex: 1,
+    alignSelf: "center",
+  },
+  title: {
+    fontSize: 38,
+    fontWeight: "900",
+    color: colors.black,
+    paddingHorizontal: 64,
+  },
+  addList: {
+    borderWidth: 2,
+    borderColor: colors.lightBlue,
+    borderRadius: 4,
+    padding: 16,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  add: {
+    color: colors.blue,
+    fontWeight: "600",
+    fontSize: 14,
+    marginTop: 8,
   },
 });
---
+++ fakeData.js
@@ -0,0 +1,80 @@
+export default fakeData = [
+  {
+    name: "List A",
+    color: "#24A6D9",
+    todos: [
+      {
+        title: "Task A",
+        completed: false,
+      },
+      {
+        title: "Task B",
+        completed: true,
+      },
+      {
+        title: "Task C",
+        completed: false,
+      },
+      {
+        title: "Task D",
+        completed: true,
+      },
+      {
+        title: "Task E",
+        completed: false,
+      },
+    ],
+  },
+  {
+    name: "List B",
+    color: "#8022D9",
+    todos: [
+      {
+        title: "Task 1",
+        completed: true,
+      },
+      {
+        title: "Task 2",
+        completed: false,
+      },
+      {
+        title: "Task 3",
+        completed: true,
+      },
+      {
+        title: "Task 4",
+        completed: false,
+      },
+      {
+        title: "Task 5",
+        completed: true,
+      },
+    ],
+  },
+  {
+    name: "List C",
+    color: "#03fcb1",
+    todos: [
+      {
+        title: "Task 1",
+        completed: true,
+      },
+      {
+        title: "Task 2",
+        completed: false,
+      },
+      {
+        title: "Task 3",
+        completed: true,
+      },
+      {
+        title: "Task 4",
+        completed: false,
+      },
+      {
+        title: "Task 5",
+        completed: true,
+      },
+    ],
+  },
+];
