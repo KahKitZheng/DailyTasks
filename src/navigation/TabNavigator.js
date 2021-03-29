f323d6acb62d85aa1c852b0db2aa598b2defd114
Kah Kit Zheng <Kahkitzheng@gmail.com>
2021-03-29 20:03:52 +0200

    Add icons to the tab navigation

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Feather } from "@expo/vector-icons";
 
 import {
   HomeNavigation,
@@ -13,7 +14,22 @@ const BottomTabNavigator = () => {
   return (
     <Tab.Navigator
       initialRouteName="Home"
-      tabBarOptions={{ style: { paddingBottom: 8 } }}
+      tabBarOptions={{
+        activeTintColor: "#000",
+        inactiveTintColor: "#C0C0C0",
+        style: { height: 48, paddingTop: 6, paddingBottom: 4 },
+      }}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          const icons = {
+            Pomodoro: "clock",
+            Home: "home",
+            Lists: "list",
+          };
+
+          return <Feather name={icons[route.name]} color={color} size={size} />;
+        },
+      })}
     >
       <Tab.Screen name="Pomodoro" component={PomdoroNavigation} />
       <Tab.Screen name="Home" component={HomeNavigation} />
